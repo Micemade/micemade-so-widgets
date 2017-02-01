ee14a23b4b50501c5875d48b33e05f978dac0455
ALEN-LT\alens <alen@micemade.com>
2017-02-01 19:48:46 +0100

    First release with Github updater

--- micemade-so-widgets.php
+++ micemade-so-widgets.php
@@ -380,3 +380,14 @@ function MM_SOW() {
 
 // Get MM_SOW Running
 MM_SOW();
+
+if( ! class_exists( 'Micemade_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+$updater = new Micemade_Updater( __FILE__ );
+$updater->set_username( 'Micemade' );
+$updater->set_repository( 'micemade-so-widgets' );
+//$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+
+$updater->initialize();
